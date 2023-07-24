970575fe319bc69bc484e77e3b484915c28eb333
gutsteip <gutsteip@tcd.ie>
2023-07-24 11:54:14 -0400

    Initial setup
    Barebones setup, import base packages

---
+++ main.py
@@ -0,0 +1,14 @@
+import webbrowser
+import requests, bs4
+
+
+def getWebData(website):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi')
+    #webbrowser.open(website)
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    website1 = 'http://ets.aeso.ca/ets_web/ip/Market/Reports/CSDReportServlet'
+    getWebData(website1)
