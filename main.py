a6df7b344371bfd99a8ac232b3a042acd02f2935
gutsteip <gutsteip@tcd.ie>
2023-07-25 16:36:36 -0400

    Modified getPoolPrice
    Modified getPoolPrice to find indices of dates in website text and find all instances of today's and yesterday's date

--- main.py
+++ main.py
@@ -1,14 +1,31 @@
-import webbrowser
-import requests, bs4
+import requests
+import time
+import pandas as pd
+import os
+from datetime import datetime, timedelta
+from bs4 import BeautifulSoup
+
+def getPoolPrice():
+    # Send a GET request to get website data and convert to string
+    response = requests.get('http://ets.aeso.ca/ets_web/ip/Market/Reports/SMPriceReportServlet')
+    websiteText = response.text
+
+    #Get dates and convert to string
+    dateTime = datetime.now()
+    todayDate = dateTime.strftime("%m/%d/%Y")
+    yesterdayDate = (dateTime - timedelta(days = 1)).strftime("%m/%d/%Y")
 
 
-def getWebData(website):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi')
-    #webbrowser.open(website)
+    #Find current day in the website's data
+    dateIndex= websiteText.find(todayDate)
+    #TODO: Get all non-null instances of today's (and previous day's dates, record price into excel (updating as needed))
+    while(dateIndex != -1):
+        dateIndex = websiteText.find(todayDate, dateIndex+1)
 
+    yestIndex= websiteText.find(yesterdayDate)
+    while(yestIndex != -1):
+        yestIndex = websiteText.find(yesterdayDate, yestIndex+1)
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    website1 = 'http://ets.aeso.ca/ets_web/ip/Market/Reports/CSDReportServlet'
-    getWebData(website1)
+    getPoolPrice()
