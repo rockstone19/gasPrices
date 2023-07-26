2009f40aa55202a3089e93e24eb7a77fe31ff4bb
gutsteip <gutsteip@tcd.ie>
2023-07-26 14:06:00 -0400

    Updated getPoolPrice
    getPoolPrice() now retrieves current data from website and stores date/hour & price during that hour in array.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import requests
 import time
 import pandas as pd
 import os
+import re
 from datetime import datetime, timedelta
 from bs4 import BeautifulSoup
 
@@ -10,21 +11,33 @@ def getPoolPrice():
     response = requests.get('http://ets.aeso.ca/ets_web/ip/Market/Reports/SMPriceReportServlet')
     websiteText = response.text
 
-    #Get dates and convert to string
-    dateTime = datetime.now()
-    todayDate = dateTime.strftime("%m/%d/%Y")
-    yesterdayDate = (dateTime - timedelta(days = 1)).strftime("%m/%d/%Y")
+    #Parse the HTML for the correct table, find all entries in said table
+    soup = BeautifulSoup(websiteText, 'html.parser')
+    table = soup.find_all('table')[2]
+    rows = table.find_all('tr')
 
+    #For storing updates in
+    hourlyPrices = []
 
-    #Find current day in the website's data
-    dateIndex= websiteText.find(todayDate)
-    #TODO: Get all non-null instances of today's (and previous day's dates, record price into excel (updating as needed))
-    while(dateIndex != -1):
-        dateIndex = websiteText.find(todayDate, dateIndex+1)
+    # Iterate over each row
+    for row in rows:
+        # Get all columns in this row
+        cols = row.find_all('td')
+        # If not the header row, extract data and append
+        if len(cols) >= 2:
+            hour = cols[0].text.strip()
+            price = cols[1].text.strip()
+            hourlyPrices.append((hour, price))
 
-    yestIndex= websiteText.find(yesterdayDate)
-    while(yestIndex != -1):
-        yestIndex = websiteText.find(yesterdayDate, yestIndex+1)
+    # Print the hours and their prices
+    for hour, price in hourlyPrices:
+        print(f'Hour: {hour}, Price: {price}')
+
+    #TODO: Combine this with TNG number, update when needed in sheet
+
+def getTNG():
+    
+
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
